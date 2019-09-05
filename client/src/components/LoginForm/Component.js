16a3a34374d209d65eab48279d78f5e05009a7b7
deeppaz <deeppaz@hotmail.com>
2019-09-06 01:40:20 +0300

    update | add forgot password, not resolve yet

--- client/src/components/LoginForm/Component.js
+++ client/src/components/LoginForm/Component.js
@@ -43,7 +43,7 @@ class LoginForm extends React.Component {
           component={renderField}
           validate={passwordValidator}
         />
-        <Link to="/forgot-password">Uye ol</Link>
+        <Link to="/forgot-password">sifremi unuttum</Link>
         <SubmitButton type='submit'>giris</SubmitButton>
       </Form>
     );
